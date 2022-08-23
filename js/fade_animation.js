92e75fc362c3660b9bee579cd1eedfa85f69520d
sarda <juliensarda1408@gmail.com>
2022-08-23 15:54:08 +0200

    animation fix

--- js/fade_animation.js
+++ js/fade_animation.js
@@ -2,9 +2,7 @@
 let carousel = document.getElementById("carousel");
 const textObserver2 = new IntersectionObserver(function (entries) {
   if (entries[0].isIntersecting === true) {
-    carousel.classList.add("animation2");
-  } else {
-    carousel.classList.remove("animation2");
+    carousel.classList.add("animation");
   }
 })
 textObserver2.observe(carousel);
@@ -14,8 +12,6 @@ let text = document.getElementById("text");
 const textObserver = new IntersectionObserver(function (entries) {
   if (entries[0].isIntersecting === true) {
     text.classList.add("animation");
-  } else {
-    text.classList.remove("animation");
-  }
+  } 
 })
 textObserver.observe(text);
