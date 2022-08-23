857eb97d215991549298fe0677662472a45fa45b
sarda <juliensarda1408@gmail.com>
2022-08-23 15:14:11 +0200

    carosel animation added

--- js/fade_animation.js
+++ js/fade_animation.js
@@ -7,3 +7,13 @@ window.addEventListener("scroll", function () {
     text.classList.remove("animation");
   }
 });
+
+// activer une animation au moment ou on scroll sur l'element
+let carousel = document.getElementById("carousel");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 1100) {
+    carousel.classList.add("animation2");
+  } else {
+    carousel.classList.remove("animation2");
+  }
+});
