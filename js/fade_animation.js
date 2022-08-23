41089db29c42d996e07fb5ea8904d416b7621c08
sarda <juliensarda1408@gmail.com>
2022-08-23 15:30:58 +0200

    js pour animation fade upgraded

--- js/fade_animation.js
+++ js/fade_animation.js
@@ -1,19 +1,21 @@
-// activer une animation au moment ou on scroll sur l'element
-let text = document.getElementById("text");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 100) {
-    text.classList.add("animation");
-  } else {
-    text.classList.remove("animation");
-  }
-});
-
-// activer une animation au moment ou on scroll sur l'element
+// activer une animation quand l'element est visible
 let carousel = document.getElementById("carousel");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 1100) {
+const textObserver2 = new IntersectionObserver(function (entries) {
+  if (entries[0].isIntersecting === true) {
     carousel.classList.add("animation2");
   } else {
     carousel.classList.remove("animation2");
   }
-});
+})
+textObserver2.observe(carousel);
+
+// activer une animation quand l'element est visible
+let text = document.getElementById("text");
+const textObserver = new IntersectionObserver(function (entries) {
+  if (entries[0].isIntersecting === true) {
+    text.classList.add("animation");
+  } else {
+    text.classList.remove("animation");
+  }
+})
+textObserver.observe(text);
